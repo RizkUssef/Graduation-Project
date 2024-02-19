33b74b63e24980c68d693b112a7e16dad7531d0f
Rizk Ussef <rizkusgt67@gmail.com>
2024-02-19 13:25:34 +0200

    push all files of the project

---
+++ Smart Tracking And Follow Up System For Kids/database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("admins")->insert([
+            "email"=>"Rizk@admin.com",
+            "password"=>bcrypt("123456")
+        ]);
+    }
+}
+//admin1 --------- Rizk@admin.com ----- 123456
+//admin2 --------- Faten@admin.com ----- 456789 
+//admin3 --------- Khadiga@admin.com ----- 789456  
